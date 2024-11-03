7cebf69d8638e1c453fc7efa297413fd80f73a82
rweide-ua <165325458+rweide-ua@users.noreply.github.com>
2024-11-03 13:42:23 -0600

    Finally begin work on this

---
+++ Fall2024-Assignment3-rweide/Models/Actor.cs
@@ -0,0 +1,20 @@
+﻿namespace Fall2024_Assignment3_rweide.Models
+{
+    public class Actor
+    {
+        string Name { get; set; }
+        string Gender { get; set; }
+        int Age { get; set; }
+        string IMDBLink { get; set; }
+        string ProfilePhoto { get; set; }
+
+        Actor()
+        {
+            Name = "Actor Name";
+            Gender = "Unknown";
+            Age = 32;
+            IMDBLink = "IMDB LINK HERE";
+            ProfilePhoto = "PHOTO LINK HERE";
+        }
+    }
+}
---
+++ Fall2024-Assignment3-rweide/Models/Movie.cs
@@ -0,0 +1,20 @@
+﻿namespace Fall2024_Assignment3_rweide.Models
+{
+    public class Movie
+    {
+        string Name { get; set; }
+        string IMDBLink { get; set; }
+        string Genre { get; set; }
+        int YearOfRelease { get; set; }
+        string PosterURL { get; set; }
+
+        Movie()
+        {
+            Name = "Movie Name";
+            IMDBLink = "IMDB URL HERE";
+            Genre = "Action";
+            YearOfRelease = 2024;
+            PosterURL = "POSTER URL HERE";
+        }
+    }
+}
