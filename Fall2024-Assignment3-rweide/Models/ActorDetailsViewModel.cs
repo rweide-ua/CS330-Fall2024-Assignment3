576d64ed27730359a55d2ab6f2663f440654312f
rweide-ua <165325458+rweide-ua@users.noreply.github.com>
2024-11-04 20:47:56 -0600

    Add overall sentiment note for actors

--- Fall2024-Assignment3-rweide/Models/ActorDetailsViewModel.cs
+++ Fall2024-Assignment3-rweide/Models/ActorDetailsViewModel.cs
@@ -7,6 +7,7 @@ namespace Fall2024_Assignment3_rweide.Models
         public Actor Actor { get; set; }
         public IEnumerable<Movie> Movies { get; set; }
         public IEnumerable<TextSentimentPair> Tweets { get; set; }
+        public readonly string OverallTweetSentiment;
 
         // NOTE: In here is where we should store the AI-generated tweets for the actor, as well as store the sentiment analysis
         // For sentiment analysis, we store the compound value? And from there, we sum all of the values up to come up with the overall sentiment
@@ -16,6 +17,26 @@ namespace Fall2024_Assignment3_rweide.Models
             Actor = actor;
             Movies = movies;
             Tweets = tweets;
+            double SentimentSum = 0.0;
+            foreach (var tweet in tweets)
+            {
+                SentimentSum += tweet.SentimentCompound;
+            }
+
+            SentimentSum /= tweets.Count();
+
+            if (SentimentSum >= 0.05)
+            {
+                OverallTweetSentiment = "Positive";
+            }
+            else if (SentimentSum > -0.05 && SentimentSum < 0.05)
+            {
+                OverallTweetSentiment = "Neutral";
+            }
+            else
+            {
+                OverallTweetSentiment = "Negative";
+            }
         }
     }
 }
